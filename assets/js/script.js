ac045b8f784b645edbfcaf6ad6209049fc5ec55b
Alan Lin <alin6120404@gmail.com>
2021-11-06 22:43:57 -0700

    pseudocoding structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,19 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // window prompt "how many letter would you like in your password?" 
+  // var passwordLetter = window.prompt()
+  // number only; so if enter integer proceed with function, else strings prompt error, return; between 8 and 128
+  // prompt = do you wish to include lowercase, uppercase, numeric, and/or special characters.
+  // at least one character type should be created
+  //generate password when all answers are answered
+  // password should either displayed in an alert or written on page.
+
+
+
+
+
+
   passwordText.value = password;
 
 }
